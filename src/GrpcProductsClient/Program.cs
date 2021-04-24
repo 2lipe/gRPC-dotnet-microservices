f5ce8bf1e84d858590ff0400895547b2d8a5d5e0
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-24 16:51:48 -0300

    chore: create method to ListAllProducts

--- src/GrpcProductsClient/Program.cs
+++ src/GrpcProductsClient/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using ProductGrpc.Protos;
 
 namespace GrpcProductsClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Waiting for server is running...");
+            Thread.Sleep(2000);
+            
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new ProductProtoService.ProductProtoServiceClient(channel);
+            
+            // GetProductAsync
+            Console.WriteLine("GetProductAsync started...");
+            var response = await client.GetProductAsync(
+                new GetProductRequest
+                {
+                    Id = 1
+                });
+
+            Console.WriteLine("GetProductAsync Response: " + response.ToString());
+            Console.ReadLine();
         }
     }
 }
