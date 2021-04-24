155f9a615544f65222891585e3e7a5bcd1584204
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-24 17:44:19 -0300

    chore: create method to insert product with grpc

--- src/GrpcProductsClient/Program.cs
+++ src/GrpcProductsClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Grpc.Net.Client;
 using ProductGrpc.Protos;
@@ -19,13 +20,14 @@ namespace GrpcProductsClient
 
             await GetProductAsync(client);
             await GetAllProductsAsync(client);
+            await AddProductAsync(client);
             
             Console.ReadLine();
         }
-        
+
         private static async Task GetProductAsync(ProductProtoService.ProductProtoServiceClient client)
         {
-            Console.WriteLine("GetProduct started...");
+            Console.WriteLine("GetProductAsync started...");
             var response = await client.GetProductAsync(
                 new GetProductRequest
                 {
@@ -37,12 +39,31 @@ namespace GrpcProductsClient
         
         private static async Task GetAllProductsAsync(ProductProtoService.ProductProtoServiceClient client)
         {
-            Console.WriteLine("GetAllProducts with C#9 started...");
+            Console.WriteLine("GetAllProductsAsync with C#9 started...");
             using var clientData = client.GetAllProducts(new GetAllProductsRequest());
             await foreach (var responseData in clientData.ResponseStream.ReadAllAsync())
             {
                 Console.WriteLine(responseData);
             }
+        }
+        
+        private static async Task AddProductAsync(ProductProtoService.ProductProtoServiceClient client)
+        {
+            Console.WriteLine("AddProductAsync started...");
+            var addProductResponse = await client.AddProductAsync(
+                new AddProductRequest
+                {
+                    Product = new ProductModel
+                    {
+                        Name = "Redmi",
+                        Description = "New Redmi smartphone",
+                        Price = 599,
+                        Status = ProductStatus.Instock,
+                        CreatedAt = Timestamp.FromDateTime(DateTime.UtcNow)
+                    }
+                });
+            
+            Console.WriteLine("AddProductAsync Response: " + addProductResponse.ToString());
         }
     }
 }
