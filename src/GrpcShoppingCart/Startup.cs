83a817961688011bf862d6ba6fc6effa101abe9d
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-24 22:11:09 -0300

    chore: create proto file shoppingcart

--- src/GrpcShoppingCart/Startup.cs
+++ src/GrpcShoppingCart/Startup.cs
@@ -38,7 +38,7 @@ namespace GrpcShoppingCart
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                //endpoints.MapGrpcService<GreeterService>();
 
                 endpoints.MapGet("/",
                     async context =>
