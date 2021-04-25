8dbf00f5b2552890e432147606f1f3698dc96715
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-24 22:35:38 -0300

    chore: create services to Create and Remove

--- src/GrpcShoppingCart/Services/ShoppingCartService.cs
+++ src/GrpcShoppingCart/Services/ShoppingCartService.cs
@@ -1,7 +1,9 @@
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Grpc.Core;
 using GrpcShoppingCart.Data;
+using GrpcShoppingCart.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ShoppingCartGrpc.Protos;
@@ -34,6 +36,61 @@ namespace GrpcShoppingCart.Services
             var shoppingCartModel = _mapper.Map<ShoppingCartModel>(shoppingCart);
 
             return shoppingCartModel;
+        }
+
+        public override async Task<ShoppingCartModel> CreateShoppingCart(ShoppingCartModel request, ServerCallContext context)
+        {
+            var shoppingCart = _mapper.Map<ShoppingCart>(request);
+
+            var isExist = await _shoppingCartContext.ShoppingCart
+                .AnyAsync(s => s.UserName == shoppingCart.UserName);
+
+            if (isExist)
+            {
+                _logger.LogError("Invalid UserName for ShoppingCart creation. UserName : {userName}", shoppingCart.UserName);
+                throw new RpcException(new Status(StatusCode.NotFound, $"ShoppingCart with Username={request.Username} is already exists"));
+            }
+
+            _shoppingCartContext.ShoppingCart.Add(shoppingCart);
+            await _shoppingCartContext.SaveChangesAsync();
+            
+            _logger.LogInformation("ShoppingCart is successfully created.Username : {userName}", shoppingCart.UserName);
+
+            var shoppingCartModel = _mapper.Map<ShoppingCartModel>(shoppingCart);
+            
+            return shoppingCartModel;
+        }
+
+        public override async Task<RemoveItemIntoShoppingCartResponse> RemoveItemIntoShoppingCart(RemoveItemIntoShoppingCartRequest request, ServerCallContext context)
+        {
+            var shoppingCart = await _shoppingCartContext.ShoppingCart
+                .FirstOrDefaultAsync(s => s.UserName == request.Username);
+
+            if (shoppingCart == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound,
+                    $"ShoppingCart with UserName={request.Username} has not found"));
+            }
+
+            var removeCartItem = shoppingCart.Items
+                .FirstOrDefault(i => i.ProductId == request.RemoveCartItem.ProductId);
+
+            if (removeCartItem == null)
+            {
+                throw new RpcException(
+                    new Status(StatusCode.NotFound,
+                        $"CartItem with ProductId={request.RemoveCartItem.ProductId} is not found in the ShoppingCart"));
+            }
+
+            shoppingCart.Items.Remove(removeCartItem);
+            var removeCount = await _shoppingCartContext.SaveChangesAsync();
+
+            var response = new RemoveItemIntoShoppingCartResponse
+            {
+                Success = removeCount > 0
+            };
+
+            return response;
         }
     }
 }
