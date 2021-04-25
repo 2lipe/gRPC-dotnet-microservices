87a4d6ceb5cea8b0cbd73d5be160195b39c7bcd4
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-25 11:55:49 -0300

    chore: add dependency injection

--- src/GrpcShoppingCart/Startup.cs
+++ src/GrpcShoppingCart/Startup.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DiscountGrpc.Protos;
 using GrpcShoppingCart.Data;
 using GrpcShoppingCart.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,11 +17,22 @@ namespace GrpcShoppingCart
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options => 
+                options.Address = new Uri(Configuration["GrpcConfigs:DiscountUrl"]));
+
+            services.AddScoped<DiscountService>();
 
             services.AddDbContext<ShoppingCartContext>(options =>
             {
